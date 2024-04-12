f6fba2d4b5928524f931a5e90f6b983fd5bd619a
mafraonia2@gmail.com <149300564+MaFraONIA@users.noreply.github.com>
2024-04-12 14:08:51 +0200

    'All Changes_V5'

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,10 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-# INPUT_REQUIRED {Provide your OpenWeather API key here}
 ENV OPENWEATHER_API_KEY=73798258221c6dcc94a6b4283fb75734
-# INPUT_REQUIRED {Set the desired latitude here}
 ENV LATITUDE=48.8534 
-# INPUT_REQUIRED {Set the desired longitude here}
 ENV LONGITUDE=2.3488 
 
 EXPOSE 8081
 
-CMD ["python", "./weather_api_wrapper.py"]
+CMD ["python", "./app.py"]
--- tests/test_app.py
+++ tests/test_app.py
@@ -9,19 +9,20 @@ class TestApp(unittest.TestCase):
         self.app.testing = True
 
     def test_fetch_weather_data_success(self):
-        response = self.app.get('/weather?lat=48.8534&lon=2.3488')
+        response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn('temperature', response.json)
-        self.assertIn('country_code', response.json)
-        self.assertIn('cloud_cover', response.json)
-        self.assertIn('humidity', response.json)
-        self.assertIn('pressure', response.json)
-        self.assertIn('wind_direction', response.json)
-        self.assertIn('wind_speed', response.json)
+        data = response.get_json()
+        self.assertIn('temperature', data)
+        self.assertIn('country_code', data)
+        self.assertIn('cloud_cover', data)
+        self.assertIn('humidity', data)
+        self.assertIn('pressure', data)
+        self.assertIn('wind_direction', data)
+        self.assertIn('wind_speed', data)
         print("Test for successful weather data fetch passed.")
 
     def test_fetch_weather_data_failure(self):
-        response = self.app.get('/weather?lat=&lon=')
+        response = self.app.get('/?lat=&lon=')
         self.assertEqual(response.status_code, 400)
         print("Test for weather data fetch failure due to missing parameters passed.")
 
